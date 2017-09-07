9b00c47c955992a106f582d7127ddf9da12ae463
I. Veselovskyy <iveselovskyy@outlook.com>
2017-09-06 20:52:16 -0400

    Working on error handling

--- eval.h
+++ eval.h
@@ -1,4 +1,17 @@
 #include "mpc.h"
 
+/* Create Enumeration of Possible Error Types */
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+/* Create Enumeration of Possible lval Types */
+enum { LVAL_NUM, LVAL_ERR };
+
+typedef struct {
+	  int type;
+	  long num;
+	      int err;
+} lval;
+
 long eval_op(long x, char* op, long y);
 long eval(mpc_ast_t* t);
+
